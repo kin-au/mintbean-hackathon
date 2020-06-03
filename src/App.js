73b79d67072c2861cb2396bb10713d66ea104861
kin-au <kinnamau@gmail.com>
2020-06-03 20:48:35 +0100

    init react app, install algoliasearch, return data from search and display on page

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
+
+const searchClient = algoliasearch(
+  "CQUXKWBDK6",
+  "4cb5729fb60da3c903c218f9783834d3"
+);
+
+const Hit = ({ hit }) => {
+  return <p>{hit.make_display}</p>;
+};
+
+function App() {
+  return (
+    <>
+      <InstantSearch searchClient={searchClient} indexName="carmakes">
+        <SearchBox />
+        <Hits hitComponent={Hit} />
+      </InstantSearch>
+    </>
+  );
+}
+
+export default App;
